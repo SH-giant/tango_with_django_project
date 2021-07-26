91ae963bfad7a4b0210d456e3df12069d389363b
SH-giant <1481874180@qq.com>
2021-07-27 00:10:32 +0800

    finish chapter4

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import os
 
 # Create your tests here.
+
+
+project_base_dir = os.getcwd()
+print(project_base_dir)
+templates_dir = os.path.join(project_base_dir, '../templates')
+print(templates_dir)
+rango_templates_dir = os.path.join(templates_dir, '')
+print(rango_templates_dir)
