e002f6b8d8ff8663d027b5fad058a183e2a4d3f6
SH-giant <1481874180@qq.com>
2021-07-28 16:00:11 +0800

    finish chapter6

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -4,9 +4,4 @@ import os
 # Create your tests here.
 
 
-project_base_dir = os.getcwd()
-print(project_base_dir)
-templates_dir = os.path.join(project_base_dir, '../templates')
-print(templates_dir)
-rango_templates_dir = os.path.join(templates_dir, '')
-print(rango_templates_dir)
+
